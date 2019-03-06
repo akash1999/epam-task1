39ed5cdae04924c3a2e92d03ed15bcd1c13073d3
Akash Chaudhary <akash.geo14@gmail.com>
2019-03-06 22:18:16 +0530

    Add: Object write functionality

--- task1/src/main/java/com/epam/task1/App.java
+++ task1/src/main/java/com/epam/task1/App.java
@@ -1,13 +1,34 @@
 package com.epam.task1;
 
+import java.io.*;
+
 /**
- * Hello world!
+ * Program to write object in a file
  *
  */
+ 
+ class Student implements Serializable{
+	 public int uid;
+	 public String name;
+	 
+	 Student(int uid, String name) {
+		this.uid = uid;
+		this.name = name;
+	 }
+ }
+ 
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+		Student student = new Student(44, "Akash Chaudhary");
+        try{
+			FileOutputStream fout = new FileOutputStream("data.dat");
+			ObjectOutputStream out = new ObjectOutputStream(fout);
+			out.writeObject(student);
+			out.close();
+			System.out.println("Success");
+		} catch(IOException e){
+			System.out.println(e);
+		}
     }
 }
